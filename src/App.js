4e000227ae92a8a10e5abd3f4bbe00d554c971bd
nancy-jin486 <liwen.jin2829@gmail.com>
2025-11-04 19:48:51 +1100

    Some Basic Text

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <>
+      <header>
+      <h1>Little Lemon</h1>
+      </header>
+      <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      </nav>
+      <main>
       <h1>hi</h1>
-    </div>
-  );
+      </main>
+      <footer>
+      <p>© 2023 Little Lemon</p>
+      </footer>
+    </>
+    );
 }
 
 export default App;
